f5c52cbccf707182016638b6e737cdd2e54ee9a8
Matthew Anders <manderson0182@gmail.com>
2025-07-14 10:51:11 -0400

    Added total for cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,10 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
+  if (cartItems.length > 0) {
+    document.querySelector(".total").innerHTML = `Total: $${total}`;
+  }
 }
 
 function cartItemTemplate(item) {
