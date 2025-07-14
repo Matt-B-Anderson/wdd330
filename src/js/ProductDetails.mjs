5af3a18338cfd3b10eb7835e5c7edad2071b4bf9
Matthew Anders <manderson0182@gmail.com>
2025-07-14 10:17:40 -0400

    Updated app to use a product list and vite to reference the list instead of every individual html file

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,47 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
+  }
+  addProductToCart(product) {
+    const cartItems = getLocalStorage("so-cart") || [];
+    const alreadyInCart = cartItems.find(
+      (cartItem) => cartItem.id === product.id,
+    );
+    if (!alreadyInCart) {
+      cartItems.push(product);
+    }
+    setLocalStorage("so-cart", cartItems);
+  }
+  renderProductDetails() {
+    productDetailsLayout(this.product);
+  }
+}
+
+function productDetailsLayout(product) {
+  document.querySelector("h2").textContent = product.Brand.Name;
+  document.querySelector("h3").textContent = product.NameWithoutBrand;
+
+  const productImage = document.getElementById("productImage");
+  productImage.src = product.Image;
+  productImage.alt = product.NameWithoutBrand;
+
+  document.getElementById("productPrice").textContent = product.FinalPrice;
+  document.getElementById("productColor").textContent =
+    product.Colors[0].ColorName;
+  document.getElementById("productDesc").innerHTML =
+    product.DescriptionHtmlSimple;
+
+  document.getElementById("addToCart").dataset.id = product.Id;
+}
