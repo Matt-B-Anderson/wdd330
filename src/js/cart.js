732db9b8c488cf54ec9d974a618ec0a68399a16b
Matthew Anders <manderson0182@gmail.com>
2025-07-18 13:11:03 -0400

    Added button to remove from the cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -8,10 +8,28 @@ function renderCartContents() {
   if (cartItems?.length > 0) {
     document.querySelector(".total").innerHTML = `Total: $${total}`;
   }
+
+  document.querySelectorAll(".remove-from-cart").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = e.currentTarget.dataset.id;
+
+      const newCart = cartItems.filter((item) => String(item.Id) !== id);
+
+      setLocalStorage("so-cart", newCart);
+      renderCartContents();
+    });
+  });
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
+  const newItem = `
+  <li class="cart-card divider">
+   <button class="remove-from-cart"
+              data-id="${item.Id}"
+              aria-label="Remove ${item.Name}">
+        ×
+      </button>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -24,7 +42,8 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+</li>
+`;
 
   return newItem;
 }
